e0dd76104707ed47fe8c924ce9e337634415a526
gergefis <votanonkipos@gmail.com>
2024-09-22 19:01:33 +0300

    Create Reactives forms

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,22 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '../common/user'; 
+import { User } from '../common/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
   private apiUrl = 'http://localhost:8080/api/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  addUser(user: User): Observable<User> {
-    return this.http.post<User>(this.apiUrl, user);
+  // addUser(user: User): Observable<User> {  //maybe needed array Observable<User[]>
+  //   return this.httpClient.post<User>(this.apiUrl, user);
+  // }
+  // getUsers(): Observable<User[]> {
+  //   return this.httpClient.get<User[]>(this.apiUrl);
+  // }
+
+  public addUser(user: User) {  //maybe needed array Observable<User[]>
+    return this.httpClient.post(this.apiUrl+ '/register-user', user);
   }
 
-  getUserById(id: number): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/${id}`);
+  public getUsers() {
+    return this.httpClient.get(this.apiUrl + '/getUsers');
   }
+
+  getUserById(id: any): Observable<User> {  // TODO
+    return this.httpClient.get<User>(`${this.apiUrl}/${id}`);
+  }
+
+  public deleteUser(id: any) { // TODO
+    return this.httpClient.delete(this.apiUrl + '/deleteUser?id=' + id);
+  }
+
+  public updateStudents(student: any) {  // TODO
+    return this.httpClient.put(this.apiUrl + '/updateUser', student);
+  }
+
+  
 }
