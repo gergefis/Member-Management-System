e242788862913b185d072d165dc3e7669c94d8f6
gergefis <votanonkipos@gmail.com>
2024-09-22 19:04:20 +0300

    change address type to Enum

--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,36 +1,44 @@
 import { Component, AfterViewInit, EventEmitter, Input, Output, ViewChild, ElementRef } from '@angular/core';
+import { Address } from '../../common/user';
 
 @Component({
   selector: 'app-address',
   templateUrl: './address.component.html',
   styleUrls: ['./address.component.css']
 })
-export class AddressComponent implements AfterViewInit {
+export class AddressComponent { //implements AfterViewInit 
 
   @Input() addressLabel: string = 'Address'; // Εισαγωγή για το label της διεύθυνσης
-  @Output() addressChange: EventEmitter<string> = new EventEmitter<string>();
+  @Output() addressChange: EventEmitter<Address> = new EventEmitter<Address>();
 
   address: string = '';
+  addressType:'WORK' | 'HOME' = 'WORK';
 
+  
   @ViewChild('addressInput', { static: false }) addressInput!: ElementRef;
 
 
-  ngAfterViewInit(): void {
-    const input = this.addressInput.nativeElement as HTMLInputElement;
-    const autocomplete = new google.maps.places.Autocomplete(input);
-
-    autocomplete.addListener('place_changed', () => {
-      const place = autocomplete.getPlace();
-      if (place.formatted_address) {
-        this.address = place.formatted_address;
-        this.addressChange.emit(this.address);
-        console.log('Selected address:', this.address);
-      }
-    });
-  }
+  // ngAfterViewInit(): void {
+  //   const input = this.addressInput.nativeElement as HTMLInputElement;
+  //   const autocomplete = new google.maps.places.Autocomplete(input);
   
-  onAddressInput(event: any) {
-    this.address = event.target.value;
-    this.addressChange.emit(this.address);
+  //   autocomplete.addListener('place_changed', () => {
+  //     const place = autocomplete.getPlace();
+  //     if (place.formatted_address) {
+  //       this.address = place.formatted_address;
+  
+  //       // Εδώ πρέπει να ορίσεις το addressType, π.χ. από επιλογή χρήστη ή default
+  //       const newAddress = new Address(this.address, this.addressType); // Βεβαιώσου ότι η addressType είναι σωστά ορισμένη
+  //       this.addressChange.emit(newAddress);
+        
+  //       console.log('Selected address:', newAddress);
+  //     }
+  //   });
+  // }
+  
+  onAddressInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.address = input.value;
+    this.addressChange.emit(new Address(this.address, this.addressType)); // Χρησιμοποιώντας Address
   }
 }
