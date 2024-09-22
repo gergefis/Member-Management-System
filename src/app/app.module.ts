799e69a9102a6c731029f96d1c739dc6ee3acee4
gergefis <votanonkipos@gmail.com>
2024-09-22 19:03:13 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material/core';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayUsersComponent } from './components/display-users/display-users.component';
-import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { HeaderComponent } from './components/header/header.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { DatepickerComponent } from './components/datepicker/datepicker.component';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { AddressComponent } from './components/address/address.component';
-
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerActions, MatDatepickerModule } from '@angular/material/datepicker';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayUsersComponent,
     RegisterUserComponent,
-    HeaderComponent,
     NavBarComponent,
-    DatepickerComponent,
+    HeaderComponent,
     AddressComponent,
+    DatepickerComponent,
+    UserListComponent,
+    DisplayUsersComponent
   ],
   imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule, 
+    MatFormFieldModule,  
+    MatInputModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatNativeDateModule,
     GoogleMapsModule,
-    BsDatepickerModule.forRoot()
-  ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatDatepickerModule
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
