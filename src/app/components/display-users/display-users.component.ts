a30c4f33e2a8c7d53618d39e9177465489300b71
gergefis <votanonkipos@gmail.com>
2024-09-27 22:58:43 +0300

    Make userDetails a stream  value

--- src/app/components/display-users/display-users.component.ts
+++ src/app/components/display-users/display-users.component.ts
@@ -1,38 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { User } from '../../common/user';
 import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-display-users',
   templateUrl: './display-users-bootstrap.component.html',
-  styleUrls: ['./display-users.component.css']
+  styleUrl: './display-users.component.css'
 })
-export class DisplayUsersComponent implements OnInit {
-
-  users: User[] = [];  //ADD data from spring
+export class DisplayUsersComponent {
+  userDetails$: Observable <User[]>;
 
   constructor(private userService: UserService) {
-    this.getUsersDetails()
-  }
-  
-  getUsersDetails() {
-    this.userService.getUsers.subscribe(
-      (resp) => {
-        console.log(resp);
-        this.studentDetails = resp;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
-  }
-
-
-
-  ngOnInit(): void {
-    this.userService.getUsers().subscribe((data: User[]) => {
-      this.users = data;
-    });
+    this.userDetails$ = this.userService.getUsers();
   }
 
 }
