9f946eb1a4d5f058dfd23b2b32e03db786f58ca6
gergefis <votanonkipos@gmail.com>
2024-09-28 22:06:52 +0300

    added user-list with id path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DisplayUsersComponent } from './components/display-users/display-users.component';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: '', redirectTo: '', pathMatch: 'full' }, 
     { path: 'display-users', component: DisplayUsersComponent },
     { path: 'register-user', component: RegisterUserComponent },
-    { path: '**', redirectTo: '/display-users' }
+    { path: 'user-list/:id', component: UserListComponent },
+    { path: '**', redirectTo: 'display-users', pathMatch: 'full' }
 ];
 
 @NgModule({
