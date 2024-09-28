f9f8d37ce104cf2e72bfeb22c68265da5d291223
gergefis <votanonkipos@gmail.com>
2024-09-28 17:57:29 +0300

    change getUsers to fetch an array object

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,41 +1,58 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { User } from '../common/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8080/api/users';
+  private apiUrl = 'http://localhost:8080/api/user';
+
+  private userId: any;
 
   constructor(private httpClient: HttpClient) { }
 
-  // addUser(user: User): Observable<User> {  //maybe needed array Observable<User[]>
-  //   return this.httpClient.post<User>(this.apiUrl, user);
+  addUser(user: User): Observable<User> {  
+    return this.httpClient.post<User>(`${this.apiUrl}/registerUser`, user);
+  }
+
+  // public addUser(user: any) {  //maybe needed array Observable<User[]>
+  //   return this.httpClient.post(this.apiUrl + '/registerUser', user);
   // }
-  // getUsers(): Observable<User[]> {
-  //   return this.httpClient.get<User[]>(this.apiUrl);
-  // }
 
-  public addUser(user: User) {  //maybe needed array Observable<User[]>
-    return this.httpClient.post(this.apiUrl+ '/register-user', user);
+  
+getUsers(): Observable<{ users: User[] }> { // Ενημερώστε την επιστροφή ώστε να περιλαμβάνει το users
+  return this.httpClient.get<{ users: User[] }>(`${this.apiUrl}/getUsers`);
+}
+
+
+
+
+  getUserById(userId: number): Observable<User> {  // TODO
+    return this.httpClient.get<User>(`${this.apiUrl}/${userId}`).pipe( 
+    catchError(error => {
+      console.error('Error fetching user by ID:', error);
+      // Μπορείς να επιστρέψεις ένα σφάλμα ή να κάνεις άλλη επεξεργασία
+      return throwError(() => new Error('Error fetching user'));
+    })
+  );
+}
+
+  public deleteUser(userId: any) { // TODO
+    return this.httpClient.delete(`${this.apiUrl}/deleteUser/${userId}`);
   }
 
-  public getUsers() {
-    return this.httpClient.get(this.apiUrl + '/getUsers');
+  public updateUser(user: any) {  // TODO
+    return this.httpClient.put(this.apiUrl + '/updateUser', user);
   }
 
-  getUserById(id: any): Observable<User> {  // TODO
-    return this.httpClient.get<User>(`${this.apiUrl}/${id}`);
+  setUserId(userId: number) {
+    this.userId = userId;
   }
 
-  public deleteUser(id: any) { // TODO
-    return this.httpClient.delete(this.apiUrl + '/deleteUser?id=' + id);
-  }
-
-  public updateStudents(student: any) {  // TODO
-    return this.httpClient.put(this.apiUrl + '/updateUser', student);
+  getUserId() {
+    return this.userId;
   }
 
   
