13e3ccdd904420cabb1f44e6c40a404b601d2020
gergefis <votanonkipos@gmail.com>
2024-09-28 22:08:45 +0300

    added user id

--- src/app/common/user.ts
+++ src/app/common/user.ts
@@ -4,37 +4,41 @@ export class User {
  public lastName: string;
  public gender: Gender;
  public birthdate: Date;
- public addresses: Address[];
+ public address: Address[]
+
 
   constructor(
+    id: number,
     firstName: string,
     lastName: string,
     gender: Gender,
     birthdate: Date,
-    addresses: Address[]
+    address: Address[]
   ) {
+    this.id =id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.gender = gender;
     this.birthdate = birthdate;
-    this.addresses = addresses;
+    this.address = address;
   }
 }
 
-export enum Gender { //TODO - Ξεχωριστή κλάση
+export enum Gender { //TODO - Ξεχωριστή κλάση?
   MALE = 'MALE',
   FEMALE = 'FEMALE'
 }
 
-export class Address { //TODO - Ξεχωριστή κλάση
+export enum AddressType {
+  HOME = 'HOME',
+  WORK = 'WORK'
+}
+
+export class Address { //TODO - Ξεχωριστή κλάση?
   address: string;
-  addressType: 'WORK' | 'HOME';
-
-
-  constructor(
-    address: string,
-    addressType: 'WORK' | 'HOME' // enum στο backend
-  ) {
+  addressType: AddressType;
+  
+  constructor(address: string, addressType: AddressType) {
     this.address = address;
     this.addressType = addressType;
   }
